0d178be5059cd1a9e715e557ede55ab16b0a6d8a
AlvaroFernandes <fernandes.alvaro@gmail.com>
2024-10-13 17:15:22 +1100

    fix mobile menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         className={`${inter.className} dark:bg-gray-950 bg-white text-gray-950 dark:text-gray-50 dark:text-opacity-90 relative pt-28 sm:pt-36`}
       >
         <div className="bg-gray-900  dark:bg-gray-100 absolute -z-10 top-[-6rem] right-[11rem] h-[11.25rem] w-[11.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
-        <div className="bg-gray-950  dark:bg-gray-300 absolute -z-10 top-[-1rem] left-[25rem] sm:left-[35rem]  h-[11.25rem] w-[11.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
+        {/* <div className="bg-gray-950  dark:bg-gray-300 absolute -z-10 top-[-1rem] left-[25rem] sm:-left-[15rem]  h-[11.25rem] w-[11.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" /> */}
         <ThemeContexProvider>
           <ActiveSectionContextProvider>
             <Header />
